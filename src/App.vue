fa1c9c8267b2587186e0ce255886558855b4ecb3
Asier <mc.asieer@gmail.com>
2025-04-03 17:39:42 +0200

    Creando interfaz UI

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,4 @@
 <template>
-
-<button class="btn btn-neutral">Neutral</button>
-<button class="btn btn-primary">Primary</button>
-<button class="btn btn-secondary">Secondary</button>
-<button class="btn btn-accent">Accent</button>
-<button class="btn btn-info">Info</button>
-<button class="btn btn-success">Success</button>
-<button class="btn btn-warning">Warning</button>
-<button class="btn btn-error">Error</button>
   
   <RouterView />
 </template>
---
+++ src/modulorum/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,16 @@
+<template>
+
+<SupraMenu />
+
+    <main>
+        <LateralisMenu />
+        <router-view />
+        
+    </main>
+</template>
+
+<script lang="ts" setup>
+import SupraMenu from '@/modulorum/commune/components/SupraMenu.vue';
+import LateralisMenu from '../components/LateralisMenu.vue';
+
+</script>
